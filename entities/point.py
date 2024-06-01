2100818a9b53c51a06144f304341f425219f14e0
Mihau13 <jastrzab.10@onet.pl>
2024-06-01 23:45:56 +0200

    Added Point superclass with time attribute.

---
+++ entities/point.py
@@ -0,0 +1,25 @@
+from sqlalchemy import *
+from typing import Self
+
+from common_base import Base
+
+class Point(Base):
+	__tablename__ = "points"
+
+	id = Column(Integer, primary_key = True)
+
+	location = Column(String(64))
+	time = Column(Integer)	# time of last contact/report
+	type = Column(String(50))
+
+	__mapper_args__ = {
+		'polymorphic_on': type,
+		'polymorphic_identity': 'point'
+	}
+
+	def __init__(self: Self, location: str = None, time: int = None):
+		self.location = location
+		self.time = time
+
+	def __repr__(self):
+		return f"Point({self.location}, {self.time})"
